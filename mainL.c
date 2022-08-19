0720da27c6c8bc8f958ef06c0e796172385e2cd7
Cylin204 <111558289+Cylin204@users.noreply.github.com>
2022-08-19 13:56:39 +0700

    Add files via upload

---
+++ mainL.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x, y;
+char ch;
+scanf("%2d %c %d",&x, &ch, &y);
+printf("%d %d %d\n",x, ch, y);
+char c1, c2, c3;
+scanf("%c%c%c",&c1, &c2, &c3);
+	return 0;
+}
